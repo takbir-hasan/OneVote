a27c4fbff9da12d7ff1df54d2366573d1df6b011
takbir-hasan <takbirhasan274@gmail.com>
2024-11-03 19:58:59 +0600

    basic architecture

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,186 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        theme: ThemeData(primarySwatch: Colors.blue),
+        darkTheme: ThemeData(primarySwatch: Colors.grey),
+        debugShowCheckedModeBanner: false,
+        home: const HomeActivity());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  mySnackBar(message, context) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+        appBar: AppBar(
+          title: const Text("OneVote"),
+          titleSpacing: 20,
+          // centerTitle: true,
+          backgroundColor: Colors.lightBlue,
+          // toolbarHeight: 60,
+          // toolbarOpacity: 1,
+          // elevation: 0,
+          actions: [
+            // IconButton(onPressed: () {}, icon: const Icon(Icons.comment)),
+            IconButton(
+                onPressed: () {
+                  mySnackBar("Searching...", context);
+                },
+                icon: const Icon(Icons.search)),
+            IconButton(
+                onPressed: () {
+                  mySnackBar("It is Settings", context);
+                },
+                icon: const Icon(Icons.settings)),
+            IconButton(
+                onPressed: () {
+                  mySnackBar("I am email", context);
+                },
+                icon: const Icon(Icons.email)),
+            IconButton(
+                onPressed: () {
+                  mySnackBar("Give me feedback", context);
+                },
+                icon: const Icon(Icons.feedback)),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: const Text("Hello Voters!"),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                  padding: const EdgeInsets.all(0),
+                  child: UserAccountsDrawerHeader(
+                    decoration: const BoxDecoration(color: Colors.lightBlue),
+                    accountName: const Text(
+                      "Sajid Hasan Takbir",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    accountEmail: const Text("takbirhasan274gmail.com"),
+                    currentAccountPicture: Image.network(
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfGQ_rvk0VOH1B9_6ikH75UH4jiEUc8uNYOQ&s"),
+                    onDetailsPressed: () {
+                      mySnackBar("Profile Page", context);
+                    },
+                  )),
+              ListTile(
+                leading: const Icon(Icons.contact_page),
+                title: const Text("Contact"),
+                onTap: () {
+                  mySnackBar("Contact Page", context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.person),
+                title: const Text("Profile"),
+                onTap: () {
+                  mySnackBar("Profile Page", context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.email),
+                title: const Text("Email"),
+                onTap: () {
+                  mySnackBar("Contact Page", context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.feedback),
+                title: const Text("Feedback"),
+                onTap: () {
+                  mySnackBar("Feedback Page", context);
+                },
+              ),
+            ],
+          ),
+        ),
+        // endDrawer: Drawer(
+        //   child: ListView(
+        //     children: [
+        //       DrawerHeader(
+        //           padding: const EdgeInsets.all(0),
+        //           child: UserAccountsDrawerHeader(
+        //             decoration: const BoxDecoration(color: Colors.lightBlue),
+        //             accountName: const Text(
+        //               "Sajid Hasan Takbir",
+        //               style: TextStyle(color: Colors.black),
+        //             ),
+        //             accountEmail: const Text("takbirhasan274gmail.com"),
+        //             currentAccountPicture: Image.network(
+        //                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfGQ_rvk0VOH1B9_6ikH75UH4jiEUc8uNYOQ&s"),
+        //             onDetailsPressed: () {
+        //               mySnackBar("Profile Page", context);
+        //             },
+        //           )),
+        //       ListTile(
+        //         leading: const Icon(Icons.contact_page),
+        //         title: const Text("Contact"),
+        //         onTap: () {
+        //           mySnackBar("Contact Page", context);
+        //         },
+        //       ),
+        //       ListTile(
+        //         leading: const Icon(Icons.person),
+        //         title: const Text("Profile"),
+        //         onTap: () {
+        //           mySnackBar("Profile Page", context);
+        //         },
+        //       ),
+        //       ListTile(
+        //         leading: const Icon(Icons.email),
+        //         title: Text("Email"),
+        //         onTap: () {
+        //           mySnackBar("Contact Page", context);
+        //         },
+        //       ),
+        //       ListTile(
+        //         leading: const Icon(Icons.feedback),
+        //         title: Text("Feedback"),
+        //         onTap: () {
+        //           mySnackBar("Feedback Page", context);
+        //         },
+        //       ),
+        //     ],
+        //   ),
+        // ),
+        // bottomNavigationBar: (),
+
+        floatingActionButton: FloatingActionButton(
+            elevation: 10,
+            backgroundColor: Colors.lightBlue,
+            onPressed: () {
+              mySnackBar("I am floating action button", context);
+            },
+            child: const Icon(Icons.add)),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.message), label: "Contact"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          ],
+          onTap: (int index) {
+            if (index == 0) {
+              mySnackBar("Home Button", context);
+            }
+            if (index == 1) {
+              mySnackBar("Message button clicked", context);
+            }
+            if (index == 2) {
+              mySnackBar("Your Profile", context);
+            }
+          },
+        ));
   }
 }

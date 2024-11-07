b8fa7e2e97a7189e144351db7d0fc7e102398d13
takbir-hasan <takbirhasan274@gmail.com>
2024-11-07 20:31:33 +0600

    login and signup

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'ProfilePage.dart';
-import 'Signup.dart';
 import 'Login.dart';
 
 Future main() async {
